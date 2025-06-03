13f4bb1b274f346c13556f9c58229d7ecb7f0a65
PranavRasal <pranavcode19@gmail.com>
2025-06-03 19:38:19 +0530

    Add Contact component and update routing in main.jsx; enhance Header links

--- src/main.jsx
+++ src/main.jsx
@@ -2,27 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {  createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
 import Home from './assets/componets/home/home.jsx'
 import About from './assets/componets/about/about.jsx'
+import Contact from './assets/componets/contact/contact.jsx'
 
-
-const router=createBrowserRouter([
-  {
-    path:"/",
-    element:<App/>,
-    children:[
-      {
-      path:"",
-      element:<Home/>,
-    },
-    {
-      path:"about",
-      element:<About/>,
-    }
-    ]
-  }
-])
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<App/>,
+//     children:[
+//       {
+//       path:"",
+//       element:<Home/>,
+//     },
+//     {
+//       path:"about",
+//       element:<About/>,
+//     }
+//     ]
+//   }
+// ])
+const router = createBrowserRouter (
+createRoutesFromElements(
+  <Route path='/' element={<App/>}>
+  <Route path='' element={<Home />}/>
+  <Route path='about' element={<About />}/>
+  <Route path='contact' element={<Contact />}/>
+  </Route>
+)
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
